11fd33fb42b02881c3b55d8d0fbf2375933a69a4
Daniel <danielkellywbs@gmail.com>
2021-04-27 22:30:41 +0300

    added login function and added next popup window

--- spendingapp.py
+++ spendingapp.py
@@ -5,6 +5,7 @@ Created on Sun Apr 25 08:11:34 2021
 @author: kingpc
 """
 from tkinter import* 
+import tkinter as tk
 import tkinter.messagebox as tkBox
 import numpy as np 
 
@@ -40,21 +41,27 @@ txtfld=Entry(window, show='*', fg='blue')
 txtfld.place(x=175, y=101)
 
 password = ["engineer", "daniel"]  
-
-
-
-
-
-
-
-
-    
-    
-    
-     
-    
-
 window.mainloop() 
+
+class App:
+    def __init__(self, root):
+        # setting title
+        root.title("CSV Graphical user interface")
+        # setting window size
+        width = 600
+        height = 600
+        screenwidth = root.winfo_screenwidth()
+        screenheight = root.winfo_screenheight()
+        alignstr = '%dx%d+%d+%d' % (width, height,
+                                    (screenwidth - width) / 2, (screenheight - height) / 2)
+        root.geometry(alignstr)
+        root.resizable(width=False, height=False)
+        
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = App(root)
+    root.mainloop()
 
   
 
