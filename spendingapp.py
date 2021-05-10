a1176836ce8e5afb21e27fa9e6b473a3649ca627
Daniel <danielkellywbs@gmail.com>
2021-05-10 14:17:05 +0300

    added transactions command, a button to submit data to database, added also another calendar

--- spendingapp.py
+++ spendingapp.py
@@ -4,11 +4,11 @@ Created on Sun Apr 25 08:11:34 2021
 
 @author: kingpc
 """
-from tkinter import * 
+from tkinter import*
 import tkinter as tk
 import tkinter.messagebox as tkBox
 import numpy as np 
-from tkcalendar import* 
+from tkcalendar import*
 import time 
 import sqlite3 
 import os 
@@ -92,47 +92,114 @@ class App:
                                     (screenwidth - width) / 2, (screenheight - height) / 2)
         root.geometry(alignstr)
         root.resizable(width=False, height=False) 
-    #%% add transaction function   
-        def addtrans(): 
-            newWin = Toplevel(root) 
-            width = 600
-            height = 400 
-            screenwidth = root.winfo_screenwidth()
-            screenheight = root.winfo_screenheight()
-            alignstr = '%dx%d+%d+%d' % (width, height,
-                                    (screenwidth - width) / 2, (screenheight - height) / 2)
-            newWin.geometry(alignstr)
-            newWin.resizable(width=False, height=False)   
-            lbl1 = Label(newWin, text="Enter amount in Euros :")
-            lbl1.place(x=180 , y=50)
+        self.mycal = Calendar(root, setmode='day', date_pattern='d/m/yy')
+        self.mycal.place(x=450, y=411)
+        
+        self.logout_btn=Button(root, text="  LOGOUT  ", fg="red", command=root.destroy)
+        self.logout_btn.place(x=600, y=50)
+        self.addtrans_btn=Button(root, text="ADD TRANSACTION", command=lambda: self.addtrans())
+        self.addtrans_btn.place(x=550, y=100)
+        self.eacc_btn=Button(root, text=" EDIT ACC.")
+        self.eacc_btn.place(x=600, y= 140)
+        self.set_bttn=Button(root, text="   SETUP    ")
+        self.set_bttn.place(x=603, y=190)
+        
+        #connect to the database
+     
+    conn = sqlite3.connect('expenses.db')
+    c = conn.cursor() 
+    #c.execute(""" CREATE TABLE MoneySpent (
+                # EUROS real, 
+                 #CATEGORY text, 
+                 #PLACE text
+                 #)""")
+        
+    conn.close()
+    
+    #%% QUERY function
+    def query(self): 
+        conn = sqlite3.connect('expenses.db')
+        c = conn.cursor() 
+        c.execute("SELECT *, oid FROM MoneySpent")
+        records =  c.fetchall()
+       # print(records) 
+        
+        print_records = ''
+        for record in records: 
+            print_records += str(record[0]) + "\n"
+        query_label = Label(self.newWin, text=print_records)
+        query_label.place(x=300, y=250)
             
-    #%% add trans function ^        
+        
+        conn.commit() 
+        conn.close() 
+     #%%   
+#%% SUBMIT FUNCTION
+    def submit(self): 
+            conn = sqlite3.connect('expenses.db') 
+            c = conn.cursor() 
+            c.execute("INSERT INTO MoneySpent VALUES(:EUROS, :CATEGORY, :PLACE)", 
+                      {
+                          'EUROS': self.euros.get(),
+                          'CATEGORY': self.cat_entry.get(), 
+                          'PLACE': self.txtfld2.get()
+            
+                      }) 
+                
+            conn.commit() 
+            conn.close() 
+    
+            self.euros.delete(0, END)  
+            self.cat_entry.delete(0,END) 
+            self.txtfld2.delete(0, END)
+           
+        
+        
+    #%% add transaction function   
+    def addtrans(self): 
+            
+        self.newWin = Toplevel(root) 
+        width = 600
+        height = 400 
+        screenwidth = root.winfo_screenwidth()
+        screenheight = root.winfo_screenheight()
+        alignstr = '%dx%d+%d+%d' % (width, height,
+                                    (screenwidth - width) / 2, (screenheight - height) / 2)
+        self.newWin.geometry(alignstr)
+        self.newWin.resizable(width=False, height=False)   
+        self.lbl1 = Label(self.newWin, text="Enter amount in Euros :", fg='red')
+        self.lbl1.place(x=150 , y=50)
+        self.euros = Entry(self.newWin, width=20)
+        self.euros.place(x=300, y=50)
+        self.lbl2 = Label(self.newWin, text="CATEGORY :", fg='green') 
+        self.lbl2.place(x=150 , y=80)
+        self.cat_entry = Entry(self.newWin, width=20)
+        self.cat_entry.place(x=299, y=80)
+        self.place_entry = Label(self.newWin, text=" ENTER NAME OF PLACE :")
+        self.place_entry.place(x=140, y=120)
+        self.txtfld2 = Entry(self.newWin, width=20)
+        self.txtfld2.place(x=299, y=120)
+        self.submit_btn=Button(self.newWin, text="SUBMIT RECORD TO DATABASE", command=lambda: self.submit())
+        self.submit_btn.place(x=280, y=160) 
+        self.showbtn=Button(self.newWin,  text="   SHOW DATA  ", command=lambda: self.query())
+        self.showbtn.place(x=280, y=200)
+            
+    #%% add trans function       
        
     #%%
         self.logout_btn=Button(root, text="  LOGOUT  ", fg="red", command=root.destroy)
         self.logout_btn.place(x=600, y=50)
-        self.addtrans_btn=Button(root, text="ADD TRANSACTION", command=addtrans)
+        self.addtrans_btn=Button(root, text="ADD TRANSACTION", command=lambda: addtrans(root))
         self.addtrans_btn.place(x=550, y=100)
         self.eacc_btn=Button(root, text=" EDIT ACC.")
         self.eacc_btn.place(x=600, y= 140)
+        self.set_bttn=Button(root, text="   SETUP    ")
+        self.set_bttn.place(x=603, y=190)
         
-        #connect to the database
-        '''
-        conn = sqlite3.connect('transaction.db')
-        c = conn.cursor() 
-        c.execute(""" CREATE TABLE transactions (
-                  rent real,
-                  travel real,
-                  groceries real,
-                  subscriptions real,
-                  cigarettes real,
-                  )""")
         
-        conn.close()
-        '''
-       
         
-      
+        
+    
         
 
 if __name__ == "__main__":
